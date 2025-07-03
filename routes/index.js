081503813c97d19b3f687b0eafa9981686c7898a
Bauti4ci <bauti.forci@gmail.com>
2025-07-03 17:07:33 -0300

    avances en ui

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'PowerUp Store' });
+});
+
+router.get('/loginform', function (req, res, next) {
+  res.render('login', { title: 'Inicio de sesión' });
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'Registro de Usuario' });
 });
 
 module.exports = router;
