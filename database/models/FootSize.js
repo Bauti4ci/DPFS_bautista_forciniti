c20802113e3348f9955bfbbe3852f4c76941a2a4
bauti_4ci <bauti.forci@gmail.com>
2025-08-28 00:53:17 -0300

    correción de modelos 6

--- database/models/FootSize.js
+++ database/models/FootSize.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     }, {
         sequelize,
-        modelName: 'Footsize',
+        modelName: 'FootSize',
         tableName: 'footsizes',
         timestamps: false
     });
--- database/models/WearSize.js
+++ database/models/WearSize.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     }, {
         sequelize,
-        modelName: 'Wearsize',
+        modelName: 'WearSize',
         tableName: 'wearsizes',
         timestamps: false
     });
