365714baeb7af195ca1e387a6cc34fb51f934bf0
bauti_4ci <bauti.forci@gmail.com>
2025-09-01 23:27:21 -0300

    problema de modelo product size

--- database/models/productsize.js
+++ database/models/productsize.js
@@ -1,21 +1,21 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-    class ProductWeight extends Model {
+    class ProductSize extends Model {
         static associate(models) {
-            ProductWeight.belongsTo(models.Product, { foreignKey: 'product_id' });
-            ProductWeight.belongsTo(models.Weight, { foreignKey: 'weight_id' });
+            ProductSize.belongsTo(models.Product, { foreignKey: 'product_id' });
+            ProductSize.belongsTo(models.Weight, { foreignKey: 'weight_id' });
         }
     }
-    ProductWeight.init({
+    ProductSize.init({
         product_id: { type: DataTypes.INTEGER, primaryKey: true },
-        weight_id: { type: DataTypes.INTEGER, primaryKey: true },
+        size_id: { type: DataTypes.INTEGER, primaryKey: true },
         stock: { type: DataTypes.INTEGER, allowNull: false }
     }, {
         sequelize,
-        modelName: 'ProductWeight',
-        tableName: 'product_weights',
+        modelName: 'ProductSize',
+        tableName: 'product_sizes',
         timestamps: false
     });
-    return ProductWeight;
+    return ProductSize;
 };
